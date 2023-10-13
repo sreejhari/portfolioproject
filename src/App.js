c7b27c8f3f53495eace76914ed5434ab5813e5c5
sreejhari <ssreejhari@gmail.com>
2023-10-13 16:09:16 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -10,13 +10,14 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-linkk"
+          
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React  
-          
+
         </a>
       </header>
     </div>
